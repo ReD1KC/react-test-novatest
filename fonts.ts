66fb2f229ee8b6787e6acbb28273d571ffb27f69
ReD1KC <tchirkok@gmail.com>
2024-08-15 08:22:41 +0700

    Init + base settings for project

---
+++ fonts.ts
@@ -0,0 +1,26 @@
+// import localFont from 'next/font/local';
+
+// export const futuraPT = localFont({
+//   src: [
+//     {
+//       path: './public/fonts/FuturaPT-Regular.woff2',
+//       weight: '400',
+//       style: 'normal',
+//     },
+//     {
+//       path: './public/fonts/FuturaPT-Italic.woff2',
+//       weight: '400',
+//       style: 'italic',
+//     },
+//     {
+//       path: './public/fonts/FuturaPT-Bold.woff2',
+//       weight: '700',
+//       style: 'normal',
+//     },
+//     {
+//       path: './public/fonts/FuturaPT-BoldItalic.woff2',
+//       weight: '700',
+//       style: 'italic',
+//     },
+//   ],
+// });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        'lg': {'min': '1440px'}, 
+        'md': {'min': '768px'},  
+        'sm': {'min': '360px'},  
+      },
+      fontFamily: {
+        sans: ['var(--font-futuraPT)'],
+      },
     },
   },
   plugins: [],
